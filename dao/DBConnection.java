2dbf9b575c0d17965dabafc5724363241da80bc2
hoboJF <jadenfiero@gmail.com>
2026-04-30 01:23:03 -0400

    changed how mysql password is handled, asks for password once when logging in on boot

--- dao/DBConnection.java
+++ dao/DBConnection.java
@@ -1,16 +1,39 @@
 package dao;
+
 import java.sql.*;
+import javax.swing.*;
 
 public class DBConnection {
     private static Connection conn = null;
     private static final String URL  = "jdbc:mysql://localhost:3306/food_delivery";
     private static final String USER = "root";
-    private static final String PASS = "password"; //change to your password
 
     public static Connection getConnection() throws SQLException {
         if (conn == null || conn.isClosed()) {
-            conn = DriverManager.getConnection(URL, USER, PASS);
+            conn = DriverManager.getConnection(URL, USER, getPassword());
         }
         return conn;
+    }
+
+    private static String password = null;
+
+    private static String getPassword() {
+        if (password == null) {
+            JPasswordField passField = new JPasswordField();
+            int result = JOptionPane.showConfirmDialog(
+                null,
+                passField,
+                "Enter MySQL root password",
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE
+            );
+            if (result == JOptionPane.OK_OPTION) {
+                password = new String(passField.getPassword());
+            } else {
+                JOptionPane.showMessageDialog(null, "Password required to run application.");
+                System.exit(0);
+            }
+        }
+        return password;
     }
 }
